0c0703ddf677c7342601670aa1e984fdbe589341
Jyoti Chaubey <jyotichaubey2005@gmail.com>
2026-06-17 08:06:54 +0000

    datatype and memory

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,23 @@
+
+///-------Memor----////
+
+//stack(primitive) and heap(non-preimitive)
+
+let myYouTubename = "jyotichaubey";
+
+
+let anothername = myYouTubename;
+anothername = "chaiandcode"
+
+console.log(myYouTubename);
+console.log(anothername);
+
+let userOne = {
+    email: "dhiajss",
+    upi:"user@yl"
+}
+let userTwo = userOne;
+ userTwo.email = "ijhdnkm";
+
+ console.log(userOne.email);
+  console.log(userTwo.email);
