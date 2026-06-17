7277f3eb2ee18c72acbe5bcd603cb95b71af349e
Jyoti Chaubey <jyotichaubey2005@gmail.com>
2026-06-17 04:31:00 +0000

    data type

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+
+
+const accountId =144;
+let accountEmail ="jyio@g,ail.com";
+var accountPassword = "1234";
+accountCity = "Jaipur";
+
+//accountId =2;
+
+accountEmail ="hfsaoifjh";
+accountPassword ="2346";
+accountCity = "delhi";
+console.table([accountId, accountEmail, accountPassword, accountCity]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+
+"use strict";
+ //treat all js code as never version
+
+ //alert("")
+ //we are using node js not brower
+
+ console.log(3+3); 
+ console.log("jyoti");
+
+ let name = "jyoti";
+ let age = 18;
+ let isLoggedIn = false
+
+ //number => 2 to power 53
+ // bigint
+ //string =""
+ //null =>standalone value
+ //undefined =>
+    //symbol= unigue
+ //object
+console.log(typeof"jyoti");
+console.log(typeof null);//object
+console.log(typeof undefined); //undefined
+
