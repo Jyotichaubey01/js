1edd4f892045804ac0d9e1973381b7ae43071358
Jyoti Chaubey <jyotichaubey2005@gmail.com>
2026-06-17 08:43:37 +0000

    string add

---
+++ 01_basics/05_string.js
@@ -0,0 +1,20 @@
+
+const name= "jyoti";
+const reoCount =50;
+//console.log ("name" +repoCount);
+//this is old version and no one like to wrtie with this
+
+console.log('hello my nmae is ${name}my repo count is ${repoCount}')
+
+const gameName = new String('jyoti');
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length());
+
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherSting = gameName.slice(-8,4);
+
+//.trim = remove space
