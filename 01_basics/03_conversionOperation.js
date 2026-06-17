e46e8879d58710cf5e47ebb830fd42e9eeaa32e7
Jyoti Chaubey <jyotichaubey2005@gmail.com>
2026-06-17 06:16:37 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,10 @@
+
+/*let score ="33abc"//null//undefined
+console.log(typeof(score));
+let valueInNumber = Number(score)
+console.log(valueInNumber) //NaN  //0 //undefined
+*/
+
+let isLoggedIn ="";//"jyoti" 
+let booleanaIsLoggedIn =Boolean(isLoggedIn)
+console.log(booleanaIsLoggedIn);//false//true
