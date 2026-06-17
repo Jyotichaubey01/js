0bc8676e03302bc052880ab909113d51c808e40d
Jyoti Chaubey <jyotichaubey2005@gmail.com>
2026-06-17 07:17:24 +0000

    conversiona and camparsion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -8,3 +8,35 @@ console.log(valueInNumber) //NaN  //0 //undefined
 let isLoggedIn ="";//"jyoti" 
 let booleanaIsLoggedIn =Boolean(isLoggedIn)
 console.log(booleanaIsLoggedIn);//false//true
+
+//*********OPERATION*******/
+
+let value = 3;
+let negValue = -value;
+//console.log(negvalue)  -3
+
+//console.log(2+2);
+
+
+let str1 = "hello"
+let str2 = "jyoti"
+
+let str3 = str1 +str2;
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log(1+"2"+2+2);
+console.log(2+1+"2");
+
+
+console.log(true) //true
+console.log(+true) //1
+
+let num1, num2, num3;
+
+num1 =num2= num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
