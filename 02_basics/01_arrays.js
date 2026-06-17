39ae4b5bdaa0af306ea531070321046ac13f76d2
Jyoti Chaubey <jyotichaubey2005@gmail.com>
2026-06-17 17:10:43 +0000

    array 1

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,24 @@
+
+// Array
+
+const myArr = [0,1,2,3,4,5]
+const myHeros = ["a","c","s"]
+
+const myArr2 =new Array(1,2,3,4)
+console.log(myArr[1]);
+
+
+//array mehtod
+myArr.pop()
+myArr.push(6)
+
+myArr.unshift(0)
+
+
+const newArr = myArr.join()
+
+
+console.log(typeof newArr);
+console.log(myArr)
+//slice and splice
+
