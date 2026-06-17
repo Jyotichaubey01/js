861141734861ec23e62efb1d114b0b5ec0443405
Jyoti Chaubey <jyotichaubey2005@gmail.com>
2026-06-17 17:32:22 +0000

    array 02

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,17 @@
+
+const marvel_heros=["d","D","w"];
+const dc_heros = ["S","D","d"];
+
+//console.log(marvel_heros);
+
+marvel_heros.concat(dc_heros)
+console.log(marvel_heros)
+
+
+const all_new_heros = [...marvel_heros, ...dc_heros]
+console.log(all_new_heros);
+
+//aray of is
+
+
+
