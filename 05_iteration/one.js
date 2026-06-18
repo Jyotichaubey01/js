3d34d54c2ca2c899e914a11bf1a9a9d858a94604
Jyoti Chaubey <jyotichaubey2005@gmail.com>
2026-06-18 16:00:14 +0000

    loop

--- 05_iteration/one.js
+++ 05_iteration/one.js
@@ -6,3 +6,4 @@ for (let index = 0; index < array.length; index++) {
     
 }
 //
+
---
+++ 05_iteration/two.js
@@ -0,0 +1,14 @@
+
+//while do while loop
+
+let index =0
+while(index<=10) {
+    console.log(`vlaue of index ${index}`);
+    index = index+2;
+}
+ //do while loop
+ let score =11
+ do {
+ console.log(`score if ${score}`);
+ score++
+ }while(score<=10)
