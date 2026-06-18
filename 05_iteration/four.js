7a328027fa1ae5a853c393e64af94c00cc7de0d8
Jyoti Chaubey <jyotichaubey2005@gmail.com>
2026-06-18 18:14:37 +0000

    loop

---
+++ 05_iteration/four.js
@@ -0,0 +1,14 @@
+
+
+const coding = ["js", "ruby", "java","python","cpp"
+
+]
+const values= coding.forEach(item) => {
+    console.log(item);
+    return item
+    
+}
+console.log(values);
+const myNUms = [1,2,3,4,5,6,7,8,9]
+myNUms.filter((num)=> num > 4)
+console.log(newNums)
---
+++ 05_iteration/three.js
@@ -0,0 +1,31 @@
+
+///
+//for of
+const arr[1,2,3,4,5]
+
+for (const num of arr) {
+    console.log(num);
+    
+    
+}
+
+//Maps=> key value pair
+
+const map= new Map()
+map.set('In',"jcso")
+
+
+    for(const key of map) {
+        console.log(key):
+    }
+    
+
+    function printMe(item){
+        console.log (item);
+    }
+    coding.forEach(printMe)
+
+    coding.forEach((item)=>{
+        console.log()
+    })
+    
