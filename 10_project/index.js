ee2687bc607651116a0d747c076395d01693cab4
Jyoti Chaubey <jyotichaubey2005@gmail.com>
2026-06-20 09:05:29 +0000

    project04

---
+++ 10_project/index.js
@@ -0,0 +1,36 @@
+
+const randomNumber= parseInt(Math.random()*100+1);
+
+const submit = document.querySelector('#subt')
+const userInput = document.querySelector('#guessField')
+const guessSlot = document.querySelector('.guesses')
+const remaining= document.querySelector('.lastResult')
+const lowOrHi= document.querySelector('.lowOrHi')
+
+let prevGuess = []
+let numGuess = 1
+
+let playGame = true;
+
+function validateGuess(guess){
+
+}
+
+function checkGuess(guess)
+{
+ 
+}
+
+function displayGuess(guess){
+    
+}
+
+function displayMessage(message){
+
+}
+
+
+
+
+
+
