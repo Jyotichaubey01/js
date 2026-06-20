3973e542190ca426b8407fd0e74fae3ca9ab9819
Jyoti Chaubey <jyotichaubey2005@gmail.com>
2026-06-20 07:44:47 +0000

    project02

---
+++ 08_project/index.js
@@ -0,0 +1,25 @@
+
+const form = document.querySelector('form')
+//this use case give you empty value
+form.addEventListener('submit',function(e)){
+    e.preventDefault()
+
+  const height=  parseInt(document.querySelector('#height').value)
+  const weight = parseInt(document.querySelector('#weight').value)
+const results = document.querySelector('#results')
+
+
+  if(height === '' || height <0 || height != isNaN(height)){
+    results.innerHTML = "please give a valid height"
+  }
+  
+else if(weight=== '' || weight<0 ||weight!= isNaN(weight)){
+    results.innerHTML = "please give a valid weight"
+  } else {
+    const bmi = (weight/((height*height)/10000).toFixeed(2))
+  ///show the result 
+  results.innerHTML = '<span>${bmi}</span>'
+  }
+
+
+});
