c47993aa85a88155483accbf2bbbc60f3bd297be
Jyoti Chaubey <jyotichaubey2005@gmail.com>
2026-06-21 23:34:23 +0530

    object and prototype

---
+++ 10_classes_and_oop/prototype.js
@@ -0,0 +1,48 @@
+
+let myName = "jyoti   "
+
+console.log(myName.length);
+
+
+let myHeros =['sf','fdg','gad']
+
+let heroPower = {
+    sf:"hammer"'
+    fgh:"sling"
+
+    getSpiderPower: function(){
+        console.log(`spidy power is ${this.spiderman}`);
+
+    }
+
+
+}
+
+Object.prototype.jyoti = function(){
+    console.log(`jyoti is peintn in all object`);
+}
+
+heroPower.jyoti()
+
+myHeros.jyoti;
+
+//inhertance
+
+const Treacher = {
+    makeVideo:true
+}
+const TreacherSupport ={
+isAviable: false
+
+__proto__:Teacher
+}
+///modern syntax
+
+Object.setPrototypeOf(TreacherSupport,Teacher)
+
+let anotherUsername = "ChaiAurCode   "
+
+String.prototype.trueLength = function(){
+    console.log(`true length: $(this.trim().length}`);
+}
+anotherUsername.truelength()
