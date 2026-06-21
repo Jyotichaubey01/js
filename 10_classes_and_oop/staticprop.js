3642a2e6b109d5a7c39c233aab85857f76d04b7f
Jyoti Chaubey <jyotichaubey2005@gmail.com>
2026-06-22 00:34:29 +0530

    class constructor

---
+++ 10_classes_and_oop/staticprop.js
@@ -0,0 +1,20 @@
+
+User(){
+    constructor(username){
+        this.user = username
+    }
+
+
+LockManager(){
+    console.log(`${this.usernmae}`);
+
+
+}
+
+createId(){
+    return `122`
+}
+}
+
+const jyoti =new User("jyoti")
+console.log(jyoti.createId())
