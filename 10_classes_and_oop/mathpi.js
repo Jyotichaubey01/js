d7c1a6e689b7691c7483702f65223f63fcee369f
Jyoti Chaubey <jyotichaubey2005@gmail.com>
2026-06-22 01:17:58 +0530

    math.pi

---
+++ 10_classes_and_oop/mathpi.js
@@ -0,0 +1,11 @@
+
+const descripter = Object.getOwnPropertyDescriptor(Math, "PI")
+
+console.log(descripter);
+
+Oconst chai = {
+    name :'ginerchai'
+    price:23
+    isAvailable:true
+}
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
